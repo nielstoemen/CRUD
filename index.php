12ee74447f5c3b5c453cc79cc397b16410e1af25
robinmulders <1190447@student.roc-nijmegen.nl>
2022-05-19 16:30:19 +0200

    design bijna af

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,300&display=swap');
     </style>
+    <script src="js/icons.js"></script>
+    
 </head>
 <body>
 
@@ -20,9 +22,11 @@
 
             <div class="navlinks">
                 <a href="contact.php">SERVICE EN CONTACT</a></li>
+                <a href="contact.php">INFO</a></li>
                 <a href="contact.php">REISEN</a></li>
                 <a href="contact.php">BOEKEN</a></li>
                 <a href="contact.php">LOGIN</a></li>
+                
             </div>
         </div>
     </div>
@@ -32,6 +36,52 @@
         <h1>BOEK HIER DE IDEAALE VAKANTIE</h1>
 
     </div>
+
+    <div class="wereldtextsectie">
+        <div class="sectietext1">
+        <img src="image/freddy.png" alt="IMAGE ERROR" height="400px" width="500px">
+        </div>
+        <div class="sectie2img">
+        <p>FAZBEAR TRAVELS TRAVELT OVER DE HELE WERELD</p>
+
+        </div>
+
+        <div class="sectie3"> 
+        <div class="slideshow-container">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="image/feddy.png" style="width:400px" height="400px">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="image/vakantieintro.jpg" style="width:400px" height="400px">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="image//freddy.png" style="width:400px"height="400px">
+  <div class="text">Caption Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
+        </div>
+      
 
 </body>
 </html>
