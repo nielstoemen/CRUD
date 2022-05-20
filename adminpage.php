d4f205eb1efddad4323156ab8075f561f6ad02fd
nielstoemen <nielstoemen@gmail.com>
2022-05-20 14:06:53 +0200

    backend login toegevoegd

---
+++ adminpage.php
@@ -0,0 +1,20 @@
+<?php 
+    include('includes/session.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>adminpage</title>
+    <link rel="stylesheet" href="css/adminstyle.css">
+</head>
+<body>
+
+
+    <br><a href="includes/logout.php">logout</a>
+
+    
+</body>
+</html>
